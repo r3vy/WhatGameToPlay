d2cc43ab3f705a038c8e73882d7c5067a3863b96
Miguel Munoz <r3vytv@gmail.com>
2014-05-01 13:46:05 -0700

    First Commit?

---
+++ whatgametoplay/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace whatgametoplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(149, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose Random Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Location = new System.Drawing.Point(149, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "None";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(149, 189);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(149, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 26);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Add Game";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.Location = new System.Drawing.Point(149, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(260, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Delete Selection";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox1.ForeColor = System.Drawing.Color.Black;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(149, 277);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(260, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackgroundImage = global::whatgametoplay.Properties.Resources.razer_logo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(559, 437);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "What Game To Play?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ whatgametoplay/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace whatgametoplay
+{
+    public partial class Form1 : Form
+    {
+        //Declare a List of Strings to be our database of games.
+        List<string> gameNames = new List<string>();
+        //Store the applications filepath + the text file to hold our games names.
+        string filePath = Application.StartupPath + "\\gameNames.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateTextPosition();
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            /*Read our game name text file and add each line from the text file to our <List>String.
+            Also adding each line from text file to our drop down box. Everything in the file should be synced with the List
+             and our Combobox Items*/
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    gameNames.Add(line);
+                    comboBox1.Items.Add(line);
+                }
+            }
+        }
+
+        //This is the Random Button. Intended to randomly pick a game from the <List>string.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            //Tells user to add a game if empty list.
+            if (gameNames.Count == 0)
+                label1.Text = "Please add a Game first!";
+            else
+                label1.Text = gameNames[rnd.Next(gameNames.Count)]; //Randomly pick a game between 0 and the number of items in the list.
+        }
+
+        //This Button will add a game to the List as a string,(what ever is in the textbox).
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string userInput;
+
+            userInput = textBox1.Text; //Users Input is taken from the textbox
+            if (textBox1.Text == "") //If it is blank, we send error message to user.
+                MessageBox.Show("Please enter a game name to add to the list");
+            else //Add the game name to the list, and to the combobox.
+            {
+                gameNames.Add(userInput);
+                comboBox1.Items.Add(userInput);
+            }
+            //Save the List to text file to ensure these games our added next time program is ran.
+            StreamWriter file = new StreamWriter(filePath);
+            gameNames.ForEach(file.WriteLine);
+            file.Close();
+            textBox1.Clear(); //Clear text for neatness. :)
+
+        }
+
+
+        //This is the Delete Button, it will delete selected items from combobox.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Error Message to user if nothing is selected.
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a game to remove from the list.");
+            }
+            else
+            {
+                gameNames.Remove(comboBox1.SelectedItem.ToString()); //Changes selected Item/Object to a string, then Removes that string from the <List>String.
+                comboBox1.Items.Remove(comboBox1.SelectedItem); //Removes selected Item from combobox listing.
+            }
+
+            //Save the List to text file to ensure these games our added next time program is ran.
+            StreamWriter file = new StreamWriter(filePath);
+            gameNames.ForEach(file.WriteLine);
+            file.Close();
+            //comboBox1.SelectedText = "";
+        }
+
+
+        //Center the title bar text. Thanks Overstack.
+        private void UpdateTextPosition()
+        {
+            Graphics g = this.CreateGraphics();
+            Double startingPoint = (this.Width / 2) - (g.MeasureString(this.Text.Trim(), this.Font).Width / 2);
+            Double widthOfASpace = g.MeasureString(" ", this.Font).Width;
+            String tmp = " ";
+            Double tmpWidth = 0;
+
+            while ((tmpWidth + widthOfASpace) < startingPoint)
+            {
+                tmp += " ";
+                tmpWidth += widthOfASpace;
+            }
+
+            this.Text = tmp + this.Text.Trim();
+        }
+    }
+}
